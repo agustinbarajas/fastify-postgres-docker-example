a4f0d305d03f4d0a4e708ab80e0fdeb901d69595
Agustin Barajas <agustinbvaldivia@gmail.com>
2021-07-03 16:52:10 -0600

    Endpoint to create users table

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,25 @@
 async function routes(fastify, options) {
-  // Testing route
-  fastify.get('/', async (request, reply) => ({ hello: 'world' }));
+  // TESTING ROUTE
+  fastify.get('/', async (request, reply) => ({ hello: 'world!' }));
+
+  // CREATE USERS TABLE IF NOT EXISTS
+  fastify.get('/initDB', (req, reply) => {
+    const onConnect = (err, client, release) => {
+      if (err) return reply.send(err);
+
+      return client.query(
+        `CREATE TABLE IF NOT EXISTS "users"
+          ("id" SERIAL PRIMARY KEY,"name" varchar(30),
+          "description" varchar(30),"tweets" integer);`,
+        (queryErr, result) => {
+          release();
+          return reply.send(queryErr || result);
+        },
+      );
+    };
+
+    fastify.pg.connect(onConnect);
+  });
 }
 
 module.exports = routes;
